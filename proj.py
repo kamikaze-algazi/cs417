583dcd5f32fb7a7dba3d574dc682666d2bf97d2e
kamikaze-algazi <kamikaze.algazi@gmail.com>
2019-05-04 05:36:23 -0400

    profile picture thumbnails in homepage

--- proj.py
+++ proj.py
@@ -124,12 +124,21 @@ class home:
                      'ORDER BY pt_time asc;')
             vars = {'em':session.email}
             posts = list(db.query(query, vars))
-            query = ('SELECT us_id, pic_name FROM "PICS"
-                      INNER JOIN ("POST" NATURAL JOIN "USER")
-                      ON prof_pic = pic_id;')
-            pics = list(db.query(vars))
-            SELECT us_id, pic_name FROM (SELECT us_id, pic_name FROM "PICS" INNER JOIN ("POST" NATURAL JOIN "USER") ON prof_pic = pic_id) AS "ALL" INNER JOIN (SELECT flwe_id FROM "FOLLOW" WHERE flwr_id = 2) AS "FLWES" ON "ALL".us_id = "FLWES".flwe_id;
-            return render_template('home.html', posts=posts,  user=session.user)
+            query = ('SELECT us_id, pic_name '
+                     'FROM (SELECT us_id, pic_name '
+                     'FROM "PICS" INNER JOIN '
+                     '("POST" NATURAL JOIN "USER") '
+                     'ON prof_pic = pic_id) AS "ALL" '
+                     'INNER JOIN '
+                     '(SELECT flwe_id FROM "FOLLOW" '
+                     'WHERE flwr_id = $uid OR flwe_id = $uid) '
+                     'AS "FLWES" ON "ALL".us_id = "FLWES".flwe_id;')
+            vars = {'uid':session.user['us_id']}
+            picQ = list(db.query(query, vars))
+            pics = dict()
+            for pic in picQ:
+                pics[pic['us_id']] = pic['pic_name']
+            return render_template('home.html', posts=posts, pics=pics, user=session.user)
         else:
             raise web.seeother('/login')
 
